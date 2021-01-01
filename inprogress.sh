813b042b926e269422cc704c1ef0cb1992e10d40
SausageMcSauce <69243388+SausageMcSauce@users.noreply.github.com>
2021-01-01 13:04:41 +0000

    start

--- inprogress.sh
+++ inprogress.sh
@@ -1,1 +1,10 @@
-echo "Carmes Linux - will be done later"
+echo "Carmes Linux - in progress"
+
+if [ -f "/etc/debian-version" ]; then # From Stack Overflow
+    echo "Carmes Linux can be installed."
+else
+    echo "Carmes Linux can only work on Debian-based systems (Only Debian will be supported for now)"
+    exit
+fi
+
+apt-get install lightdm openbox nautilus gedit gnome-terminal lxpanel
