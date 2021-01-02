6f9e23fbe338d885d7df7cec2f912ea79d9eb533
SausageMcSauce <69243388+SausageMcSauce@users.noreply.github.com>
2021-01-02 18:34:49 +0000

    more progress

--- inprogress.sh
+++ inprogress.sh
@@ -1,10 +1,16 @@
 echo "Carmes Linux - in progress"
 
+if [ `whoami` != root ]; then # script from electrictoolbox.com
+    echo "Please run as superuser or sudo"
+    exit
+fi
 if [ -f "/etc/debian-version" ]; then # From Stack Overflow
     echo "Carmes Linux can be installed."
 else
-    echo "Carmes Linux can only work on Debian-based systems (Only Debian will be supported for now)"
+    echo "Carmes Linux can only work on Debian-based systems (Only Debian Buster will be supported for now)"
     exit
 fi
 
-apt-get install lightdm openbox nautilus gedit gnome-terminal lxpanel
+apt-get install lightdm openbox nautilus gedit gnome-terminal lxpanel chromium # might add option for firefox
+
+cat ./data/autostart.sh >> /etc/xdg/openbox/autostart # this part might not work at first
